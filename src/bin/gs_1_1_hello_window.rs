f1969d9e1a0ab1d497fe6dd857ee088ab8de2f7a
Benjamin Wasty <benny.wasty@gmail.com>
2017-06-04 21:42:27 +0200

    1.1: few more steps

--- src/bin/gs_1_1_hello_window.rs
+++ src/bin/gs_1_1_hello_window.rs
@@ -1,9 +1,35 @@
 extern crate glfw;
+use self::glfw::Context;
 
+extern crate gl;
+use self::gl::types::*;
+
+// settings
+const SCR_WIDTH: u32 = 800;
+const SCR_HEIGHT: u32 = 600;
+
+#[allow(dead_code)]
 fn main() {
+    // glfw: initialize and configure
+    // ------------------------------
     let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
     glfw.window_hint(glfw::WindowHint::ContextVersion(3, 3));
     glfw.window_hint(glfw::WindowHint::OpenGlProfile(glfw::OpenGlProfileHint::Core));
     #[cfg(target_os = "macos")]
     glfw.window_hint(glfw::WindowHint::OpenGlForwardCompat(true));
+
+    // glfw window creation
+    // --------------------
+    let (mut window, events) = glfw.create_window(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", glfw::WindowMode::Windowed)
+        .expect("Failed to create GLFW window");
+
+    window.make_current();
+
+    // TODO?: glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    // gl: load all OpenGL function pointers
+    // ---------------------------------------
+    gl::load_with(|symbol| window.get_proc_address(symbol) as *const _);
+
+    unsafe { gl::Viewport(0, 0, SCR_WIDTH as i32, SCR_HEIGHT as i32) }
 }
