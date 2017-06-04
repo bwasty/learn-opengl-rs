bb9f49235481f107d3e044192b7edbffb44e6d10
Benjamin Wasty <benny.wasty@gmail.com>
2017-06-04 23:14:20 +0200

    finish first tutorial

---
+++ src/bin/gs_1_2_hello_window_clear.rs
@@ -0,0 +1,72 @@
+extern crate glfw;
+use self::glfw::{ Context, Key, Action };
+
+extern crate gl;
+
+use std::sync::mpsc::Receiver;
+
+// settings
+const SCR_WIDTH: u32 = 800;
+const SCR_HEIGHT: u32 = 600;
+
+#[allow(dead_code)]
+fn main() {
+    // glfw: initialize and configure
+    // ------------------------------
+    let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
+    glfw.window_hint(glfw::WindowHint::ContextVersion(3, 3));
+    glfw.window_hint(glfw::WindowHint::OpenGlProfile(glfw::OpenGlProfileHint::Core));
+    #[cfg(target_os = "macos")]
+    glfw.window_hint(glfw::WindowHint::OpenGlForwardCompat(true));
+
+    // glfw window creation
+    // --------------------
+    let (mut window, events) = glfw.create_window(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", glfw::WindowMode::Windowed)
+        .expect("Failed to create GLFW window");
+
+    window.make_current();
+    window.set_key_polling(true);
+    window.set_framebuffer_size_polling(true);
+
+    // gl: load all OpenGL function pointers
+    // ---------------------------------------
+    gl::load_with(|symbol| window.get_proc_address(symbol) as *const _);
+
+    unsafe { gl::Viewport(0, 0, SCR_WIDTH as i32, SCR_HEIGHT as i32) }
+
+    // render loop
+    // -----------
+    while !window.should_close() {
+        // events
+        // -----
+        process_events(&mut window, &events);
+
+        // render
+        // ------
+        unsafe {
+            gl::ClearColor(0.2, 0.3, 0.3, 1.0);
+            gl::Clear(gl::COLOR_BUFFER_BIT);
+        }
+
+        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+        // -------------------------------------------------------------------------------
+        window.swap_buffers();
+        glfw.poll_events();
+    }
+}
+
+fn process_events(window: &mut glfw::Window, events: &Receiver<(f64, glfw::WindowEvent)>) {
+    for (_, event) in glfw::flush_messages(&events) {
+        match event {
+            glfw::WindowEvent::FramebufferSize(width, height) => {
+                // make sure the viewport matches the new window dimensions; note that width and 
+                // height will be significantly larger than specified on retina displays.
+                unsafe { gl::Viewport(0, 0, width, height) }
+            },
+            glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => {
+                window.set_should_close(true)
+            },
+            _ => {},
+        }
+    }
+}
--- src/bin/mod.rs
+++ src/bin/mod.rs
@@ -1,2 +1,2 @@
 // Workaround to get the RLS working on the binaries
-pub mod gs_1_1_hello_window;
+pub mod gs_1_2_hello_window_clear;
