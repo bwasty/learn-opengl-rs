bcba43464963e697f1770d7066cf1edd212ba225
Benjamin Wasty <benny.wasty@gmail.com>
2017-06-05 09:30:23 +0200

    finish porting 2.1 (slightly wrong output)

--- src/bin/gs_2_1_hello_triangle.rs
+++ src/bin/gs_2_1_hello_triangle.rs
@@ -55,10 +55,10 @@ fn main() {
     // ---------------------------------------
     gl::load_with(|symbol| window.get_proc_address(symbol) as *const _);
 
-    // build and compile our shader program
-    // ------------------------------------
-    // vertex shader
-    unsafe {
+    let (shaderProgram, VAO) = unsafe {
+        // build and compile our shader program
+        // ------------------------------------
+        // vertex shader
         let vertexShader = gl::CreateShader(gl::VERTEX_SHADER);
         let c_str_vert = CString::new(vertexShaderSource.as_bytes()).unwrap();
         gl::ShaderSource(vertexShader, 1, &c_str_vert.as_ptr(), ptr::null());
@@ -103,10 +103,10 @@ fn main() {
         // set up vertex data (and buffer(s)) and configure vertex attributes
         // ------------------------------------------------------------------
         let vertices = [
-            -0.5, -0.5, 0.0, // left  
-             0.5, -0.5, 0.0, // right 
-             0.0,  0.5, 0.0  // top   
-        ]; 
+            -0.5, -0.5, 0.0, // left
+             0.5, -0.5, 0.0, // right
+             0.0,  0.5, 0.0  // top
+        ];
         let (mut VBO, mut VAO) = (0, 0);
         gl::GenVertexArrays(1, &mut VAO);
         gl::GenBuffers(1, &mut VBO);
@@ -115,25 +115,26 @@ fn main() {
 
         gl::BindBuffer(gl::ARRAY_BUFFER, VBO);
         gl::BufferData(
-            gl::ARRAY_BUFFER, 
-            (vertices.len() * mem::size_of::<GLfloat>()) as GLsizeiptr, 
-            mem::transmute(&vertices[0]), 
+            gl::ARRAY_BUFFER,
+            (vertices.len() * mem::size_of::<GLfloat>()) as GLsizeiptr,
+            mem::transmute(&vertices[0]),
             gl::STATIC_DRAW);
 
         gl::VertexAttribPointer(0, 3, gl::FLOAT, gl::FALSE, 3 * mem::size_of::<GLfloat>() as GLsizei, ptr::null());
         gl::EnableVertexAttribArray(0);
 
         // note that this is allowed, the call to gl::VertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
-        gl::BindBuffer(gl::ARRAY_BUFFER, 0); 
+        gl::BindBuffer(gl::ARRAY_BUFFER, 0);
 
         // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
         // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
-        gl::BindVertexArray(0); 
+        gl::BindVertexArray(0);
 
         // uncomment this call to draw in wireframe polygons.
         // gl::PolygonMode(gl::FRONT_AND_BACK, gl::LINE);
-    }
 
+        (shaderProgram, VAO)
+    };
 
     // render loop
     // -----------
@@ -147,6 +148,12 @@ fn main() {
         unsafe {
             gl::ClearColor(0.2, 0.3, 0.3, 1.0);
             gl::Clear(gl::COLOR_BUFFER_BIT);
+
+            // draw our first triangle
+            gl::UseProgram(shaderProgram);
+            gl::BindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
+            gl::DrawArrays(gl::TRIANGLES, 0, 3);
+            // glBindVertexArray(0); // no need to unbind it every time
         }
 
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
@@ -160,7 +167,7 @@ fn process_events(window: &mut glfw::Window, events: &Receiver<(f64, glfw::Windo
     for (_, event) in glfw::flush_messages(&events) {
         match event {
             glfw::WindowEvent::FramebufferSize(width, height) => {
-                // make sure the viewport matches the new window dimensions; note that width and 
+                // make sure the viewport matches the new window dimensions; note that width and
                 // height will be significantly larger than specified on retina displays.
                 unsafe { gl::Viewport(0, 0, width, height) }
             },
