dc777d367004ba0bb07cd88534b7b9e5c5ae3d2b
Benjamin Wasty <benny.wasty@gmail.com>
2017-06-10 15:25:24 +0200

    finish camera class

--- src/camera.rs
+++ src/camera.rs
@@ -10,12 +10,14 @@ type Vector3 = cgmath::Vector3<f32>;
 type Matrix4 = cgmath::Matrix4<f32>;
 
 // Defines several possible options for camera movement. Used as abstraction to stay away from window-system specific input methods
+#[derive(PartialEq)]
 pub enum Camera_Movement {
     FORWARD,
     BACKWARD,
     LEFT,
     RIGHT
 }
+use self::Camera_Movement::*;
 
 // Default camera values
 const YAW: f32        = -90.0;
@@ -26,7 +28,7 @@ const ZOOM: f32       =  45.0;
 
 struct Camera {
     // Camera Attributes
-    pub Position: Vector3,
+    pub Position: Point3,
     pub Front: Vector3,
     pub Up: Vector3,
     pub Right: Vector3,
@@ -41,7 +43,7 @@ struct Camera {
 }
 
 impl Camera {
-    fn new(position: Vector3, up: Vector3, yaw: f32, pitch: f32) -> Camera {
+    fn new(position: Point3, up: Vector3, yaw: f32, pitch: f32) -> Camera {
         Camera {
             Position: position,
             WorldUp: up,
@@ -52,6 +54,63 @@ impl Camera {
     }
 
     // TODO? constructor with scalar values
+
+    /// Returns the view matrix calculated using Eular Angles and the LookAt Matrix
+    pub fn GetViewMatrix(&self) -> Matrix4 {
+        Matrix4::look_at(self.Position, self.Position + self.Front, self.Up)
+    }
+
+    /// Processes input received from any keyboard-like input system. Accepts input parameter in the form of camera defined ENUM (to abstract it from windowing systems)
+    pub fn ProcessKeyboard(&mut self, direction: Camera_Movement, deltaTime: f32) {
+        let velocity = self.MovementSpeed * deltaTime;
+        if direction == FORWARD {
+            self.Position += self.Front * velocity;
+        }
+        if direction == BACKWARD {
+            self.Position += -(self.Front * velocity);
+        }
+        if direction == LEFT {
+            self.Position += -(self.Right * velocity);
+        }
+        if direction == RIGHT {
+            self.Position += self.Right * velocity;
+        }
+    }
+
+    /// Processes input received from a mouse input system. Expects the offset value in both the x and y direction.
+    pub fn ProcessMouseMovement(&mut self, mut xoffset: f32, mut yoffset: f32, constrainPitch: bool) {
+        xoffset *= self.MouseSensitivity;
+        yoffset *= self.MouseSensitivity;
+
+        self.Yaw   += xoffset;
+        self.Pitch += yoffset;
+
+        // Make sure that when pitch is out of bounds, screen doesn't get flipped
+        if constrainPitch {
+            if self.Pitch > 89.0 {
+                self.Pitch = 89.0;
+            }
+            if self.Pitch < -89.0 {
+                self.Pitch = -89.0;
+            }
+        }
+
+        // Update Front, Right and Up Vectors using the updated Eular angles
+        self.updateCameraVectors();
+    }
+
+    // Processes input received from a mouse scroll-wheel event. Only requires input on the vertical wheel-axis
+    pub fn ProcessMouseScroll(&mut self, yoffset: f32) {
+        if self.Zoom >= 1.0 && self.Zoom <= 45.0 {
+            self.Zoom -= yoffset;
+        }
+        if self.Zoom <= 1.0 {
+            self.Zoom = 1.0;
+        }
+        if self.Zoom >= 45.0 {
+            self.Zoom = 45.0;
+        }
+    }
 
     /// Calculates the front vector from the Camera's (updated) Eular Angles
     fn updateCameraVectors(&mut self) {
@@ -71,7 +130,7 @@ impl Camera {
 impl Default for Camera {
     fn default() -> Camera {
         let mut camera = Camera {
-            Position: Vector3::zero(),
+            Position: Point3::new(0.0, 0.0, 0.0),
             Front: vec3(0.0, 0.0, -1.0),
             Up: Vector3::zero(),        // initialized later
             Right: Vector3::zero(),     // initialized later
