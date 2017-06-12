03e4e0fe3e25ceb199e8583c17be2efd530ab9d8
Benjamin Wasty <benny.wasty@gmail.com>
2017-06-12 23:09:36 +0200

    2.2.2 basic lighting specular

--- src/_2_lighting/mod.rs
+++ src/_2_lighting/mod.rs
@@ -1,6 +1,6 @@
-mod _1_colors;
-pub use self::_1_colors::*;
-mod _2_1_basic_lighting_diffuse;
-pub use self::_2_1_basic_lighting_diffuse::*;
-// mod _2_1_basic_lighting_specular;
-// pub use self::_2_1_basic_lighting_specular::*;
+// mod _1_colors;
+// pub use self::_1_colors::*;
+// mod _2_1_basic_lighting_diffuse;
+// pub use self::_2_1_basic_lighting_diffuse::*;
+mod _2_2_basic_lighting_specular;
+pub use self::_2_2_basic_lighting_specular::*;
