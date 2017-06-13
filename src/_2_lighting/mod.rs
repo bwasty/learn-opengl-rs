147e68945781f0f2980b9310ac83cf964724ba26
Benjamin Wasty <benny.wasty@gmail.com>
2017-06-13 19:34:58 +0200

    prepare 2.4

--- src/_2_lighting/mod.rs
+++ src/_2_lighting/mod.rs
@@ -6,3 +6,7 @@ mod _2_2_basic_lighting_specular;
 pub use self::_2_2_basic_lighting_specular::*;
 mod _3_1_materials;
 pub use self::_3_1_materials::*;
+mod _4_1_lighting_maps_diffuse_map;
+pub use self::_4_1_lighting_maps_diffuse_map::*;
+// mod _4_2_lighting_maps_specular_map;
+// pub use self::_4_2_lighting_maps_specular_map::*;
