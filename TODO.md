6fad7375bbef7bbf39fd0b953096d1d18956d65f
Benjamin Wasty <benny.wasty@gmail.com>
2017-06-17 12:23:18 +0200

    add 1.1.1 hello window

--- TODO.md
+++ TODO.md
@@ -1,8 +1,7 @@
-* add 1.1 after all?
 * shader structure - first number missing...move to src/_1_getting_started
 * remove redundant shaders for subsequent tutorials?
 * Vector3::new() -> vec3()
 * adapt old event/input handling code
+* update readme!
 
 * port 7.1 debugging (at least glCheckError macro)
-* port 2.lighting
---
+++ src/_1_getting_started/_1_1_hello_window.rs
@@ -0,0 +1,60 @@
+extern crate glfw;
+use self::glfw::{Context, Key, Action};
+
+extern crate gl;
+
+use std::sync::mpsc::Receiver;
+
+// settings
+const SCR_WIDTH: u32 = 800;
+const SCR_HEIGHT: u32 = 600;
+
+pub fn main_1_1_1() {
+    // glfw: initialize and configure
+    // ------------------------------
+    let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
+    glfw.window_hint(glfw::WindowHint::ContextVersion(3, 3));
+    glfw.window_hint(glfw::WindowHint::OpenGlProfile(glfw::OpenGlProfileHint::Core));
+    #[cfg(target_os = "macos")]
+    glfw.window_hint(glfw::WindowHint::OpenGlForwardCompat(true));
+
+    // glfw window creation
+    // --------------------
+    let (mut window, events) = glfw.create_window(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", glfw::WindowMode::Windowed)
+        .expect("Failed to create GLFW window");
+
+    window.make_current();
+    window.set_key_polling(true);
+    window.set_framebuffer_size_polling(true);
+
+    // gl: load all OpenGL function pointers
+    // ---------------------------------------
+    gl::load_with(|symbol| window.get_proc_address(symbol) as *const _);
+
+    // render loop
+    // -----------
+    while !window.should_close() {
+        // events
+        // -----
+        process_events(&mut window, &events);
+
+        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+        // -------------------------------------------------------------------------------
+        window.swap_buffers();
+        glfw.poll_events();
+    }
+}
+
+fn process_events(window: &mut glfw::Window, events: &Receiver<(f64, glfw::WindowEvent)>) {
+    for (_, event) in glfw::flush_messages(events) {
+        match event {
+            glfw::WindowEvent::FramebufferSize(width, height) => {
+                // make sure the viewport matches the new window dimensions; note that width and
+                // height will be significantly larger than specified on retina displays.
+                unsafe { gl::Viewport(0, 0, width, height) }
+            }
+            glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => window.set_should_close(true),
+            _ => {}
+        }
+    }
+}
