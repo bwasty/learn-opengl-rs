4cfb0c2db7cbbfc671d8ef0df20da49b7b9503c1
Benjamin Wasty <benny.wasty@gmail.com>
2017-07-12 10:09:10 +0200

    "finish" 4.10.1, but not working

--- src/_4_advanced_opengl/_10_1_instancing_quads.rs
+++ src/_4_advanced_opengl/_10_1_instancing_quads.rs
@@ -1,42 +1,27 @@
 #![allow(non_upper_case_globals)]
 #![allow(non_snake_case)]
 
+use std::ptr;
 use std::mem;
 use std::os::raw::c_void;
-use std::ffi::CStr;
 
 extern crate glfw;
 use self::glfw::Context;
 
 extern crate gl;
+use self::gl::types::*;
 
-use cgmath::{Matrix4, Deg, Vector2, vec3, Point3, perspective};
+use cgmath::{Vector2};
 
 use num::range_step;
 
-use common::{process_events, processInput};
 use shader::Shader;
-use camera::Camera;
 
 // settings
 const SCR_WIDTH: u32 = 1280;
 const SCR_HEIGHT: u32 = 720;
 
-// TODO: copied from 4.10.1
 pub fn main_4_10_1() {
-    let mut camera = Camera {
-        Position: Point3::new(0.0, 0.0, 3.0),
-        ..Camera::default()
-    };
-
-    let mut firstMouse = true;
-    let mut lastX: f32 = SCR_WIDTH as f32 / 2.0;
-    let mut lastY: f32 = SCR_HEIGHT as f32 / 2.0;
-
-    // timing
-    let mut deltaTime: f32; // time between current frame and last frame
-    let mut lastFrame: f32 = 0.0;
-
     // glfw: initialize and configure
     // ------------------------------
     let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
@@ -47,22 +32,17 @@ pub fn main_4_10_1() {
 
     // glfw window creation
     // --------------------
-    let (mut window, events) = glfw.create_window(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", glfw::WindowMode::Windowed)
+    let (mut window, _events) = glfw.create_window(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", glfw::WindowMode::Windowed)
         .expect("Failed to create GLFW window");
 
     window.make_current();
     window.set_framebuffer_size_polling(true);
-    window.set_cursor_pos_polling(true);
-    window.set_scroll_polling(true);
-
-    // tell GLFW to capture our mouse
-    window.set_cursor_mode(glfw::CursorMode::Disabled);
 
     // gl: load all OpenGL function pointers
     // ---------------------------------------
     gl::load_with(|symbol| window.get_proc_address(symbol) as *const _);
 
-    let (shader, ) = unsafe {
+    let (shader, quadVAO, quadVBO) = unsafe {
         // configure global opengl state
         // -----------------------------
         gl::Enable(gl::DEPTH_TEST);
@@ -99,49 +79,68 @@ pub fn main_4_10_1() {
             gl::STATIC_DRAW);
         gl::BindBuffer(gl::ARRAY_BUFFER, 0);
 
-        // TODO!!!
+        // set up vertex data (and buffer(s)) and configure vertex attributes
+        // ------------------------------------------------------------------
+        let quadVertices: [f32; 30] = [
+            // positions   // colors
+            -0.05,  0.05,  1.0, 0.0, 0.0,
+             0.05, -0.05,  0.0, 1.0, 0.0,
+            -0.05, -0.05,  0.0, 0.0, 1.0,
 
-        (shader, )
+            -0.05,  0.05,  1.0, 0.0, 0.0,
+             0.05, -0.05,  0.0, 1.0, 0.0,
+             0.05,  0.05,  0.0, 1.0, 1.0
+        ];
+        let (mut quadVAO, mut quadVBO) = (0, 0);
+        gl::GenVertexArrays(1, &mut quadVAO);
+        gl::GenBuffers(1, &mut quadVBO);
+        gl::BindVertexArray(quadVBO);
+        gl::BindBuffer(gl::ARRAY_BUFFER, quadVBO);
+        gl::BufferData(gl::ARRAY_BUFFER,
+                       (quadVertices.len() * mem::size_of::<GLfloat>()) as GLsizeiptr,
+                       &quadVertices[0] as *const f32 as *const c_void,
+                       gl::STATIC_DRAW);
+        gl::EnableVertexAttribArray(0);
+        let stride = 5 * mem::size_of::<GLfloat>() as GLsizei;
+        gl::VertexAttribPointer(0, 2, gl::FLOAT, gl::FALSE, stride, ptr::null());
+        gl::EnableVertexAttribArray(1);
+        gl::VertexAttribPointer(1, 3, gl::FLOAT, gl::FALSE, stride, (2 * mem::size_of::<GLfloat>()) as *const c_void);
+        // also set instance data
+        gl::EnableVertexAttribArray(2);
+        gl::BindBuffer(gl::ARRAY_BUFFER, instanceVBO); // this attribute comes from a different vertex buffer
+        gl::VertexAttribPointer(2, 2, gl::FLOAT, gl::FALSE, (2 * mem::size_of::<GLfloat>()) as i32, ptr::null());
+        gl::BindBuffer(gl::ARRAY_BUFFER, 0);
+        gl::VertexAttribDivisor(2, 1); // tell OpenGL this is an instanced vertex attribute.
+
+        (shader, quadVAO, quadVBO)
     };
 
     // render loop
     // -----------
     while !window.should_close() {
-        // per-frame time logic
-        // --------------------
-        let currentFrame = glfw.get_time() as f32;
-        deltaTime = currentFrame - lastFrame;
-        lastFrame = currentFrame;
-
-        // events
-        // -----
-        process_events(&events, &mut firstMouse, &mut lastX, &mut lastY, &mut camera);
-
-        // input
-        // -----
-        processInput(&mut window, deltaTime, &mut camera);
-
         // render
         // ------
         unsafe {
             gl::ClearColor(0.1, 0.1, 0.1, 1.0);
             gl::Clear(gl::COLOR_BUFFER_BIT | gl::DEPTH_BUFFER_BIT);
 
-            // configure transformation matrices
-            let projection: Matrix4<f32> = perspective(Deg(45.0), SCR_WIDTH as f32 / SCR_HEIGHT as f32, 0.1, 100.0);
-            let view = camera.GetViewMatrix();
-            let mut model = Matrix4::<f32>::from_translation(vec3(0.0, -1.75, 0.0)); // translate it down so it's at the center of the scene
-            model = model * Matrix4::from_scale(0.2);  // it's a bit too big for our scene, so scale it down
+            // draw 100 instanced quads
             shader.useProgram();
-            shader.setMat4(c_str!("projection"), &projection);
-            shader.setMat4(c_str!("view"), &view);
-            shader.setMat4(c_str!("model"), &model);
-
+            gl::BindVertexArray(quadVAO);
+            gl::DrawArraysInstanced(gl::TRIANGLES, 0, 6, 100); // 100 triangles of 6 vertices each
+            gl::BindVertexArray(0);
         }
 
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
         // -------------------------------------------------------------------------------
         window.swap_buffers();
         glfw.poll_events();
+    }
+
+        // optional: de-allocate all resources once they've outlived their purpose:
+    // ------------------------------------------------------------------------
+    unsafe {
+        gl::DeleteVertexArrays(1, &quadVAO);
+        gl::DeleteBuffers(1, &quadVBO);
     }
 }
