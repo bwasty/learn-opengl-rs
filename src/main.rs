48bcc8178c96eeeae4468c38388229d94c9aad70
Benjamin Wasty <benny.wasty@gmail.com>
2017-08-01 08:51:22 +0200

    WIP 5.6 hdr

--- src/main.rs
+++ src/main.rs
@@ -106,6 +106,7 @@ fn main() {
         #[cfg(feature = "chapter-5")] "5_1" => main_5_1(),
         #[cfg(feature = "chapter-5")] "5_2" => main_5_2(),
         #[cfg(feature = "chapter-5")] "5_4" => main_5_4(),
+        #[cfg(feature = "chapter-5")] "5_6" => main_5_6(),
 
         #[cfg(feature = "chapter-7")] "7_1" => main_7_1(),
 
